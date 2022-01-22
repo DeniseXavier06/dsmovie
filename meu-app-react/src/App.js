7374b2ef5c2e96ae2fd8ba4f1d8a2808cb24390d
DeniseXavier06 <denisexavier062019@gmail.com>
2022-01-22 10:09:57 -0300

    Routes

--- meu-app-react/src/App.js
+++ meu-app-react/src/App.js
@@ -1,12 +1,27 @@
 
-import Navbar from "./componentes/navbar";
 
 
-function App() {
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+  } from "react-router-dom";
+  import Listing from './componentes/pages/Listing';
+  import Form from './componentes/pages/Form';
+  import Navbar from "./componentes/navbar";
+  function App() {
   return (
-  <Navbar />
-  
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+    </Route>
+    </Routes>
+  </BrowserRouter>
   );
-}
-
-export default App;
+  }
+  export default App;
+  
